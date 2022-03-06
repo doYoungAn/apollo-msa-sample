7d462af3f78d8a7d30c2202821c9d9f756876dfb
doYoungAn <jispoop@gmail.com>
2022-03-06 23:00:40 +0900

    리드미 생성

---
+++ test-apollo-server/README.md
@@ -0,0 +1,1 @@
+# Test Apollo Server
--- test-apollo-server/src/resolvers/item.ts
+++ test-apollo-server/src/resolvers/item.ts
@@ -6,9 +6,10 @@ const resolvers = {
          * * 아이템 타입에 대한 리졸버를 구현한 부분
          * * 해당 함수 안에서 rest로 데이터를 요창한뒤 받은 값을 그대로 리턴한다.
          * * 리턴된 데이터가 결국 graphql 요청에 대한 값으로 전달된다.
+         * @link https://www.apollographql.com/docs/apollo-server/data/resolvers/#resolver-arguments
          * @returns 
          */
-        items: async () => {
+        items: async (parent, args, context, info) => {
             const response = await fetch('http://localhost:8000/api/items', { method: 'POST' });
             const data = await response.json();
             return data;
